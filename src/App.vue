98915ff77e7cf3bdb8b59233c3df77fc3f87b934
Postnikov <mikhail.postnikov@sap.com>
2025-01-19 22:35:20 +0100

    Header layout added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <!-- eslint-disable no-debugger -->
 <script setup>
-import MyHello from '@/components/MyHello.vue'
-import { ref } from 'vue'
-
-const puma = ref({
-  name: "Puma",
-  price: 123000
-});
+  import Header from "@/components/Header.vue";
 
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <MyHello :name="puma.name" :price="puma.price"></MyHello>
-    </div>
-    <button @click="changeName">Change Name</button>
-  </header>
+  <Header />
 </template>
 
 <style scoped>
