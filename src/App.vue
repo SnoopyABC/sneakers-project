bf5b9465a34d67e1f3af05cb096cacea21d36555
Postnikov <mikhail.postnikov@sap.com>
2025-01-19 18:43:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable no-debugger -->
+<script setup>
+import MyHello from '@/components/MyHello.vue'
+import { ref } from 'vue'
+
+const puma = ref({
+  name: "Puma",
+  price: 123000
+});
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <MyHello :name="puma.name" :price="puma.price"></MyHello>
+    </div>
+    <button @click="changeName">Change Name</button>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sneakers-list-item mb-5">
+    <h3>Name: {{ name }}</h3>
+    <div>Price: {{ price }}</div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    name: {
+      type: String,
+      default: "None",
+    },
+    price: {
+      type: Number,
+      default: 0
+    }
+  })
+</script>
+
+<style scoped>
+  .sneakers-list-item{
+    border: 1px solid #000;
+    padding: 10px;
+    border-radius: 5px;
+  }
+</style>
