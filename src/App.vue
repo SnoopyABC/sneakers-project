3ccd185425d6da453255375ec6d2c37f805cf3db
Postnikov <mikhail.postnikov@sap.com>
2025-01-20 15:47:35 +0100

    Added input and select

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,33 @@ import Drawer from '@/components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
-  <Header />
-  <CardList />
+  <div class="bg-white w-3/5 m-auto rounded-xl shadow-xl shadow-grey-200 mt-14">
+    <Header />
+    <!-- <Drawer /> -->
+    <div class="p-10">
+      <div class="flex justify-between items-center mb-8">
+        <h2 class="text-3xl font-bold">Все кросовки</h2>
+        <div class="flex gap-4">
+          <select
+          class="py-2 px-3 border border-gray-200 focus:border-gray-400 rounded-md focus:outline-none"
+        >
+          <option value="name">По названию</option>
+          <option value="price">По цене (дешевые)</option>
+          <option value="price">По цене (дорогие)</option>
+        </select>
+        <div class="relative">
+          <img src="/search.svg" class="absolute left-4 top-3" alt="" />
+          <input
+            placeholder="Поиск..."
+            class="border border-gray-200 rounded-md py-2 pl-10 pr-4 outline-none"
+          />
+        </div>
+        </div>
+
+      </div>
+    </div>
+    <CardList />
+  </div>
 </template>
 
 <style scoped></style>
